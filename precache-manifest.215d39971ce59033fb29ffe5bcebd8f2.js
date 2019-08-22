9db174c3bc3c04425a0550572b1a2d9d4ee75dff
Ulyanova Anastasia <uljanova.anastasia@gmail.com>
2019-08-22 20:03:32 +0300

    Updates

---
+++ precache-manifest.215d39971ce59033fb29ffe5bcebd8f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc748648c5b69d320d8cceca7c4a1ac3",
+    "url": "/app/public/index.html"
+  },
+  {
+    "revision": "62fe57171d7a021162b3",
+    "url": "/app/public/static/js/2.a45ebe0f.chunk.js"
+  },
+  {
+    "revision": "58a8f3ce31868322023e",
+    "url": "/app/public/static/js/main.2d6c2a11.chunk.js"
+  },
+  {
+    "revision": "03884586c7f662daf3a4",
+    "url": "/app/public/static/js/runtime~main.3705c75e.js"
+  }
+]);
